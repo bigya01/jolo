b45fef298451663ce849bf7fb4a44aaab9ac8035
bishal0602 <bishalpanta60@gmail.com>
2022-12-11 03:09:16 +0545

    fixed dashboard view logic

--- jolo/dashboard/urls.py
+++ jolo/dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard_view, location_view, users_view, services_view, billing_view, target_view
+from .views import dashboard_view, location_view, users_view, services_view, billing_view, target_view, marketing_view
 
 
 urlpatterns = [
     path('<slug:shop_slug>/', dashboard_view, name="dashboard"),
+    path('<slug:shop_slug>/marketing/', marketing_view, name="marketing"),
     path('<slug:shop_slug>/map/', location_view, name="location"),
     path('<slug:shop_slug>/users/', users_view, name="users"),
     path('<slug:shop_slug>/services/', services_view, name="services"),
