5f3728487e2cd1c41827d6383343b91aaf261b25
bishal0602 <bishalpanta60@gmail.com>
2022-12-10 12:18:37 +0545

    Added Models

--- jolo/shop/models.py
+++ jolo/shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+from phone_field import PhoneField    # TODO: pip install django-phone-field
 
-# Create your models here.
+
+class Shop(models.Model):
+    #address from Google API
+    shop_name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50) # use slugify to populate from shop_name
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    
+class Service(models.Model):
+    service_name = models.CharField(max_length=50)
+    duration = models.TimeField()
+    shop = models.ForeignKey(Shop, related_name='shop_service', on_delete=models.CASCADE)
+
+class Client(models.Model):
+    client_name = models.CharField(max_length=50)
+    client_email = models.EmailField(max_length=254)
+    client_phone = models.PhoneField(blank=True, help_text='Contact phone number')
+    # shop = models.ForeignKey(Shop, related_name='shop_client', on_delete=models.CASCADE)
+
+
+APPOINTMENT_STATUS_CHOICES =(
+    ("Pending", "Pending"),
+    ("Confirmed", "Confirmed"),
+    ("Cancelled", "Cancelled"),
+)
+class Appointment(models.Model):
+    appointment_time = models.DateTimeField()
+    appointment_status = models.CharField(max_length=10, choices=APPOINTMENT_STATUS_CHOICES, default="Pending")
+    service = models.ForeignKey(Service, related_name='service_appointment', on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, related_name='shop_appointment', on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, related_name='client_appointment', on_delete=models.CASCADE)
