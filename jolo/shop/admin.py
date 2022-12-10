b142de615b6d9680fd448984c2069e32a4c5b9fe
bishal0602 <bishalpanta60@gmail.com>
2022-12-10 12:44:29 +0545

    Registered models to admin

--- jolo/shop/admin.py
+++ jolo/shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Shop)
+class PostAdmin(admin.ModelAdmin):
+    list_display =['shop_name', 'slug', 'user']
+    search_fields = ['shop_name', 'user']
+    prepopulated_fields = {'slug': ('shop_name',)}
+
+@admin.register(models.Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display =['service_name', 'duration', 'shop']
+    search_fields = ['service_name', 'shop']
+
+@admin.register(models.Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display =['client_name', 'client_email', 'client_phone']
+    search_fields = ['client_name', 'client_email', 'client_phone']
+
+@admin.register(models.Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display =['appointment_time', 'appointment_status','choices' ,'service', 'shop', 'client']
+    search_fields = ['appointment_time', 'appointment_status', 'choices','service', 'shop', 'client']
+
